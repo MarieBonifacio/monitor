d7d143f9f06b95c824ae16ceb75e6097a7e3d9a6
MarieBonifacio <mariebonifacio.pro@gmail.com>
2020-11-12 11:32:56 +0100

    fix(index): title

--- src/index.js
+++ src/index.js
@@ -1,16 +1,44 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const os = require('os-utils');
+const iconPath = path.join(__dirname, "icon", "monitor.png");
+const log = require('electron-log');
+const {autoUpdater} = require("electron-updater");
+
+require('update-electron-app')({
+  repo: 'MarieBonifacio/monitor',
+  updateInterval: '5 minutes',
+  logger: require('electron-log')
+});
+
+//-------------------------------------------------------------------
+// Logging
+//
+// THIS SECTION IS NOT REQUIRED
+//
+// This logging setup is not required for auto-updates to work,
+// but it sure makes debugging easier :)
+//-------------------------------------------------------------------
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
 }
 
+
+
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: iconPath,
+    webPreferences: {
+      nodeIntegration: true
+    }
   });
 
   // and load the index.html of the app.
@@ -18,6 +46,16 @@ const createWindow = () => {
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
+
+  setInterval(() => {
+    os.cpuUsage(function(v){
+      mainWindow.webContents.send('cpu',v*100);
+      mainWindow.webContents.send('mem',os.freememPercentage()*100);
+      mainWindow.webContents.send('total-mem',os.totalmem()/1024);
+      mainWindow.webContents.send('free-mem',os.freemem());
+    });
+  },1000);
+
 };
 
 // This method will be called when Electron has finished
@@ -33,6 +71,64 @@ app.on('window-all-closed', () => {
     app.quit();
   }
 });
+
+//-------------------------------------------------------------------
+// Open a window that displays the version
+//
+// THIS SECTION IS NOT REQUIRED
+//
+// This isn't required for auto-updates to work, but it's easier
+// for the app to show a window than to have to click "About" to see
+// that updates are working.
+//-------------------------------------------------------------------
+let win;
+
+function sendStatusToWindow(text) {
+  log.info(text);
+  win.webContents.send('message', text);
+}
+function createDefaultWindow() {
+  win = new BrowserWindow();
+  win.webContents.openDevTools();
+  win.on('closed', () => {
+    win = null;
+  });
+  win.loadURL(`file://${__dirname}/version.html#v${app.getVersion()}`);
+  return win;
+}
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+})
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+})
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+  sendStatusToWindow('Update downloaded');
+});
+
+
+//-------------------------------------------------------------------
+// Auto updates - Option 1 - Simplest version
+//
+// This will immediately download an update, then install when the
+// app quits.
+//-------------------------------------------------------------------
+app.on('ready', function()  {
+  autoUpdater.checkForUpdatesAndNotify();
+});
+
 
 app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
